07ac2d9dd7d1c35304fb10a31595ebd2237e4efc
Jordi Muntada <jmuntada@gmail.com>
2024-02-27 12:37:50 +0100

    now sms is enable

--- app.js
+++ app.js
@@ -1,13 +1,65 @@
+
+
 const express = require('express');
+
 const app = express();
 const expressSitemapXml = require('express-sitemap-xml');
 const glob = require('glob');
 const path = require('path');
 
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+require('dotenv').config();
+
+
 const PORT = process.env.PORT || 3000;
+
+
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+/* Send SMS */
+app.use(bodyParser.json());
+
+app.post('/send-sms', async (req, res) => {
+    const url = "https://api.gateway360.com/api/3.0/sms/send";
+    const data = req.body; // Assuming the body of the request to your server contains the data for the SMS API
+    const now = new Date();
+    const sendAt = now.toISOString().replace('T', ' ').substring(0, 19);
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "api_key": process.env.API_KEY_SMSPUBLI, // Use the API key from environment variables
+                "report_url": "http://ethics.land/callback/script",
+                "concat": 1,
+                "messages": [
+                    {
+                        "from": "NoChorizo",
+                        "to": "0034626381615",
+                        "text": `Hola Jordi, Has recibido un suscritor nuevo: ${data.email}`,
+                        "send_at": sendAt
+                    }
+                ]
+            }),
+            
+            
+            
+            //JSON.stringify(data),
+        });
+
+        const responseData = await response.json();
+        res.send(responseData);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+/******************* */
 
 // Function to get URLs for the sitemap
 async function getUrls() {
